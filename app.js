0d602836ca33d134d25ef331f941e7f39a0a6b1c
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-08-23 15:15:34 +0530

    updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cors = require("cors")
 connectDatabase();
 
 app.use(cors({
-  origin:["http://localhost:5173"]
+  origin:["http://localhost:5173", "http://localhost:5175"]
 }))
 
 app.use(express.json());
