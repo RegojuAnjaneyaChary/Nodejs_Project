a3610d0914951cc5d15cf6a9807ecaf85baed307
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-08-25 11:45:14 +0530

    testing weather manager issue is fixed

--- Routes/managerRoutes.js
+++ Routes/managerRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const Router = express.Router();
-const {createTicket, getAllTickets, getTicketsById, getAllemployees}= require("../Controllers/managersControllers.js") 
-const{checkAuth, checkRole}= require("../Middlewares/authMiddleware.js")
-const {tokenValidator, validateMiddleware}= require("../Validators/authValidators.js")
+const { createTicket, getAllTickets, getTicketsById, getAllemployees } = require("../Controllers/managersControllers.js")
+const { checkAuth, checkRole } = require("../Middlewares/authMiddleware.js")
+const { tokenValidator, validateMiddleware } = require("../Validators/authValidators.js")
 
-Router.get("/getAllemployeeList", tokenValidator, validateMiddleware, checkAuth, checkRole("managers"), getAllemployees)
-Router.post("/create", tokenValidator, validateMiddleware, checkAuth, checkRole("managers"), createTicket);
-Router.get("/allTickets", tokenValidator, validateMiddleware, checkAuth, checkRole("managers"), getAllTickets);
-Router.get("/ticketByID/:ticketID", tokenValidator, validateMiddleware,  checkAuth, checkRole("managers"), getTicketsById);
+Router.get("/getAllemployeeList", tokenValidator, validateMiddleware, checkAuth, checkRole("manager"), getAllemployees)
+Router.post("/create", tokenValidator, validateMiddleware, checkAuth, checkRole("manager"), createTicket);
+Router.get("/allTickets", tokenValidator, validateMiddleware, checkAuth, checkRole("manager"), getAllTickets);
+Router.get("/ticketByID/:ticketID", tokenValidator, validateMiddleware, checkAuth, checkRole("manager"), getTicketsById);
 module.exports = Router;
