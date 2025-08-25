a1adbcb4d42519fd3165ea2403bffde16711ded4
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-08-25 16:17:18 +0530

    cors updated

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const cors = require("cors")
 connectDatabase();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5175"], // allowed origins
-  credentials: true // this allows cookies, sessions, and Authorization headers
+  // origin: ["http://localhost:5173", "http://localhost:5175"], // allowed origins
+  // credentials: true // this allows cookies, sessions, and Authorization headers
+  //full access 
 }));
 
 
