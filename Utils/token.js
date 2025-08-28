274775cec5527e1a3bb7ce69173d8f1c88ea70d2
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-08-28 12:32:19 +0530

    token edited

--- Utils/token.js
+++ Utils/token.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 async function generateToken(user, req, res, next) {
     try {
-        const token = await jwt.sign({id:user._id, name:user.name}, process.env.jwt_secret_key, {expiresIn:"24h"})
+        const token = await jwt.sign({id:user._id, name:user.name || user.username,}, process.env.jwt_secret_key, {expiresIn:"24h"})
         return token;
     } catch (error) {
         console.log(error);
