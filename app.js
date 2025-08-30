a545e70cae9956a7d49049852f2aecf6d52a7525
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-08-30 13:40:35 +0530

    mail added in create task

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(cors({
     "http://localhost:5175",
     "https://task-manager-green-nine.vercel.app"
   ],
-  credentials: true
+  credentials: true,
+
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
